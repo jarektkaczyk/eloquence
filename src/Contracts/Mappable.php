85d2f6b92624f0bd2184b06b5c42c6d31eb1c9e4
Jarek Tkaczyk <jarek@softonsofa.com>
2015-03-28 11:47:56 +0100

    fix mappable contract

--- src/Contracts/Mappable.php
+++ src/Contracts/Mappable.php
@@ -2,8 +2,9 @@
 
 interface Mappable
 {
+    public function getAttribute($key);
+    public function setAttribute($key, $value);
     public function hasMapping($key);
-    public function mapAttribute($key);
     public function getMappingForAttribute($key);
     public function getMaps();
     public function setMaps(array $mappings);
